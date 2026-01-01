b3ac6412b616ecdb4d85bd9a1e29701fafc9df2c
Harpanada <liyovanh@gmail.com>
2026-01-01 23:32:38 +0700

    Making Transcribe Module/Function

---
+++ src/transcribe.py
@@ -0,0 +1,13 @@
+import whisper
+import json
+from pathlib import Path
+
+
+def Transcribe(video_path: Path , out_path: Path) -> str:
+    model = whisper.load_model("small")
+    result= model.transcribe(video_path,word_timestamps=True)
+
+    Path(out_path).write_text(json.dumps(result, indent=2))
+    return result
+
+
